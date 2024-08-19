f3c6452089723d4bb5162bd8c3dd310de62742fa
rezanahi <raad.thunder1@gmail.com>
2024-08-19 19:24:41 +0330

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+let flag = 1
+
+if(localStorage.getItem('theme') === 'dark'){
+    let body = document.getElementById('body')
+    body.classList.add('dark-mode')
+    flag = 0;
+}
+
+let hamburger = document.getElementById('hamburger')
+hamburger.addEventListener('click', showMenu)
+
+let closeBtn = document.getElementById('mobile-menu-close')
+closeBtn.addEventListener('click', hideMenu)
+
+let mobileMenuList = document.getElementById('mobile-menu-list')
+mobileMenuList.addEventListener('click', hideMenu)
+
+function showMenu() {
+    let mobileMenu = document.getElementById("mobile-menu")
+    mobileMenu.style.display = 'flex'
+}
+
+function hideMenu() {
+    let mobileMenu = document.getElementById('mobile-menu')
+    mobileMenu.style.display = 'none'
+}
+
+let theme = document.getElementById('menu__item-sun')
+theme.addEventListener('click', darkMode)
+
+function darkMode() {
+    let body = document.getElementById('body')
+    body.classList.toggle('dark-mode')
+
+    if(flag === 1){
+        localStorage.setItem('theme', 'dark')
+        flag = 0
+    } else {
+        localStorage.setItem('theme', 'light')
+        flag = 1
+    }
+}
